0fb70aaf84c30745eb2f6274ba2e6f1b5db7c91a
nehagavde <82151152+nehagavde@users.noreply.github.com>
2023-07-02 19:34:23 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,47 +2,36 @@ import streamlit as st
 import joblib
 
 def main():
-     st.markdown("<h1 style='text-align: center; color:white;'>Health Insurance Cost Prediction</h1>",
-                 unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color:white;'>Health Insurance Cost Prediction</h1>", unsafe_allow_html=True)
+    st.image('https://emerj.com/wp-content/uploads/2018/10/predictive-analytics-in-healthcare-current-applications-and-trends-3.jpg')
 
-     st.image('https://emerj.com/wp-content/uploads/2018/10/predictive-analytics-in-healthcare-current-applications-and-trends-3.jpg')
+    model = joblib.load('model_gradient_boosting_regressor (1).joblib')
 
-     model = joblib.load('model_gradient_boosting_regressor (1)')
+    age = st.slider('Enter Your Age', 18, 100)
 
-     age = st.slider('Enter Your Age',18,100)
+    sex_mapping = {'Male': 1, 'Female': 0}
+    sex = st.selectbox('Sex', ('Male', 'Female'))
+    sex = sex_mapping[sex]
 
-     sex = st.selectbox('Sex',('Male','Female'))
-     if(sex=='Male'):
-          sex=1
-     else:
-          sex=0
+    bmi = st.number_input("Enter Your BMI Value")
 
-     bmi = st.number_input("Enter Your BMI Value")
+    children = st.selectbox('Enter Number of Children', (0, 1, 2, 3, 4))
 
-     children = st.selectbox('Enter Number of Children',(0,1,2,3,4))
+    smoker_mapping = {'Yes': 1, 'No': 0}
+    smoker = st.selectbox('Smoker', ('Yes', 'No'))
+    smoker = smoker_mapping[smoker]
 
-     smoker = st.selectbox('Smoker',('Yes','No'))
-     if(smoker=='Yes'):
-          smoker=1
-     else:
-          smoker=0
+    region_mapping = {'Southwest': 1, 'Southeast': 2, 'Northwest': 3, 'Northeast': 4}
+    region = st.selectbox('Enter Your Region', ('Southwest', 'Southeast', 'Northwest', 'Northeast'))
+    region = region_mapping[region]
 
-     region = st.selectbox('Enter Your Region',('Southwest','Southeast','Northwest','Northeast'))
-     if(region=='Southwest'):
-          region=1
-     elif(region=='Southeast'):
-          region=2
-     elif(region=='Northwest'):
-          region=3
-     else:
-          region=4
+    if st.button('Predict'):
+        predicted_cost = model.predict([[age, sex, bmi, children, smoker, region]])
 
-     if st.button('Predict'):
-          pred = model.predict([[age, sex, bmi, children, smoker, region]])
-
-          st.balloons()
-          st.success('Your Insurance Cost is {}'.format(round(pred[0],2)))
-
+        st.balloons()
+        st.success('Your Insurance Cost is {}'.format(round(predicted_cost[0], 2)))
 
 if __name__ == '__main__':
-     main()
+    main()
+
+  
